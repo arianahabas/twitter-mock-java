e3a4de8999f6a5afffa588f1aed8cfcda4436393
adam <adam.j.arambula@gmail.com>
2024-02-27 18:54:02 -0500

    Added <Type, ID> to all repositories.

--- src/main/java/com/cooksys/social_media_api/repositories/UserRepository.java
+++ src/main/java/com/cooksys/social_media_api/repositories/UserRepository.java
@@ -1,8 +1,9 @@
 package com.cooksys.social_media_api.repositories;
 
+import com.cooksys.social_media_api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository { // add <TYPE, ID>
+public interface UserRepository extends JpaRepository<User, Long> {
 }
