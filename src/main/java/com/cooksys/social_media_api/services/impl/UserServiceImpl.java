bc33aa468d6d84a955197e8c329c16afb26aa9cd
arianahabas <arianahabas@gmail.com>
2024-02-26 18:57:38 -0600

    UserService and UserServiceImpl added

---
+++ src/main/java/com/cooksys/social_media_api/services/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.cooksys.social_media_api.services.impl;
+
+import com.cooksys.social_media_api.services.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
